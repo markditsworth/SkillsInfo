d1d14adad5a8f19a55e21e8c6946ace702675b14
Mark Ditsworth <markditsworth@protonmail.com>
2020-04-05 13:03:05 -0500

    rework looping

--- python/scraper.py
+++ python/scraper.py
@@ -15,6 +15,11 @@ python scraper.py --username <your linkedin username> --password <your linkedin 
 
 --driver-path defaults to /usr/local/bin/chromedriver
 --login-url defaults to the login page as of April 5, 2020
+
+To Do:
+- Add comments!
+- scrape info from page
+- kafka producer
 """
 import argparse
 
@@ -56,39 +61,49 @@ class LinkedInScraper:
         password_input = self.browser.find_element_by_id("password")
         password_input.send_keys(password)
         
-        #XPATH="/html/body/div/main/div/form/div[3]/button"//*[@id="app__container"]/main/div/form/div[3]/button
-        #XPATH="/html/body/div/main/div/form/div[conatins(@class,'login__form_action_container')]/button"
-        #div[contains(@class, 'Caption') and text()='Model saved']
         XPATH="/html/body/div/main/div/form/div[contains(@class, 'login__form_action_container')]/button"
         login_button = self.browser.find_element_by_xpath(XPATH)
         login_button.click()
     
     def searchForRelevantLIProfiles(self, query, page_limit=3):
-        count = 0
-        users_links = []
+        count = 0           # for keeping track of pagination
+        users_links = []    # initialize empty list for urls to user pages
+        
         self.browser.get("https://google.com")
+        
+        # enter query into Google search bar, limiting to linkedin.com/in/ sites
         search_bar = self.browser.find_element_by_name('q')
         query = "site:linkedin.com/in/ AND " + query
         search_bar.send_keys(query)
-        
+        # submit query
         search_button = self.browser.find_element_by_xpath("/html/body/div/div[4]/form/div[2]/div[1]/div[3]/center/input[1]")
         search_button.click()
         
-        while count < page_limit:
+        # iterate through google serach pages
+        while True:
+            # get all search results
             results = self.browser.find_elements_by_class_name('r')
+            
+            # organize all hrefs to the users_links list
             users_links.extend([result.find_element_by_tag_name('a').get_attribute('href') for result in results])
+            
             # go to next page
             next_button = self.browser.find_element_by_id('pnnext')
             next_page_link = next_button.get_attribute('href')
             self.browser.get(next_page_link)
             count += 1
+            if count == page_limit:
+                break
         
+        print(users_links)
         return users_links
+    
+        
 
 if __name__ == '__main__':
     username, password, driverpath, url = parseArgs()
     LIS = LinkedInScraper(username, password, driverpath, url)
     LIS.loginToLinkedIn()
-    LIS.searchLI('"computer vision"')
+    LIS.searchForRelevantLIProfiles('"computer vision"')
 
 
