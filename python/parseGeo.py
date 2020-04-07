e9a078d1acf33b85b7461dca79c4a9a0ba22de31
Mark Ditsworth <markditsworth@protonmail.com>
2020-04-07 17:09:44 -0500

    clean up code and add commentary

--- python/parseGeo.py
+++ python/parseGeo.py
@@ -5,10 +5,6 @@ Created on Mon Apr  6 21:17:54 2020
 
 @author: markd
 
-To Do:
---- Kafka Consumer
--/- Add geo_point field (https://www.elastic.co/guide/en/elasticsearch/reference/7.6/geo-point.html)
--/- Kafka Producer
 """
 
 import secret
@@ -21,8 +17,7 @@ from kafka import KafkaConsumer
 
 def argParse():
     parser = argparse.ArgumentParser(description='Parse LinkedIn Skills Data')
-    parser.add_argument('--topic', type=str,
-                        help='topic to read from')
+    parser.add_argument('--topic', type=str, help='topic to read from')
     parser.add_argument('--host', type=str, default="localhost", help="Kafka hostname")
     parser.add_argument('--port', type=int, default=29092, help="Kafka port number")
     parser.add_argument('--from-file', action='store_true', help="Read from file instead of kafka")
@@ -31,6 +26,10 @@ def argParse():
     parsed_topic = 'parsed-' + raw_topic
     return args.topic, parsed_topic, args.host, args.port, args.from_file
 
+'''
+Queries Google Geocode API to convert addresses to latitude/longitude
+  Requires the api_key from secret.py
+'''
 def getLatLong(location, api_key):
     url = "https://maps.googleapis.com/maps/api/geocode/json"
     params = {"address": location, "key": api_key}
@@ -40,42 +39,69 @@ def getLatLong(location, api_key):
     long= info['results'][0]['geometry']['location']['lng']
     return lat, long
 
+'''
+Accepts info dictionary, performs the conversion of address to coordinates, and
+  returns new dictionary with the coordinates field, formatted for elasticsearch
+  geo_point type. If not available, default coordinates to 0,0
+'''
 def enrichLocation(info, api_key):
     enriched_info = info.copy()
     if info['location'] != 'Other':
         lat, long = getLatLong(info['location'],api_key)
-        enriched_info['coordinates'] = {'lat': lat, 'lon': long}
+        enriched_info['coordinates'] = {'lat': lat, 'lon': long} # formatted for ES geo_point data type
+    
+    # if LinkedIn user put "Other" for location, default to (0,0)
     else:
         enriched_info['coordinates'] = {'lat': 0, 'lon': 0}
     return enriched_info
 
+'''
+Serializes dictionary for publishing to kafka
+'''
 def serialize(dictionary):
         return json.dumps(dictionary).encode('ascii')
     
 if __name__ == '__main__':
     topic, parsed_topic, host, port, from_file = argParse()
+    
+    # Setup Kafka producer
     producer = KafkaProducer(bootstrap_servers=["{}:{}".format(host,port)])
     if not from_file:
+        # Setup Kafka consumer
         consumer = KafkaConsumer(topic, group_id='{}-consumer-group-1'.format(topic),
                                       bootstrap_servers=["{}:{}".format(host,port)],
                                       auto_offset_reset='earliest',
                                       value_deserializer=lambda m: json.loads(m.decode('ascii')))
+        
+        # for each message in the kafka topic
         for m in consumer:
             message= m.value
             print(message)
-            enriched_message = enrichLocation(message, secret.geocoder_api_key)
-            producer.send(parsed_topic, serialize(enriched_message))
             
+            # add coordinates field to message
+            enriched_message = enrichLocation(message, secret.geocoder_api_key)
+            # send to parsed- topic
+            producer.send(parsed_topic, serialize(enriched_message))
+    
+    # if reading from file instead of kafka
     else:
-        path = "{}_output.json".format(topic)    
+        # get file name
+        path = "{}_output.json".format(topic)  
+        
+        # read contents of file, splitting by line
         with open(path,'r') as fObj:
             content = fObj.readlines()
         
+        # for each document
         for x in content:
+            # convert to dict
             data = json.loads(x)
             print(data['location'])
+            # add coordinates field
             enriched_data = enrichLocation(data, secret.geocoder_api_key)
             # send to kafka
             producer.send(parsed_topic, serialize(enriched_data))
+        
+        # flush messages to ensure they are written to kafka
         producer.flush()
         print("Messages from {} sent.".format(path))
--- python/scraper.py
+++ python/scraper.py
@@ -24,36 +24,28 @@ import time
 import random
 import json
 import selenium
-from selenium import webdriver 
-#from selenium.webdriver.common.by import By 
-#from selenium.webdriver.support.ui import WebDriverWait 
-#from selenium.webdriver.support import expected_conditions as EC 
-#from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from selenium import webdriver
 from kafka import KafkaProducer
 from hashlib import sha256
 
 def parseArgs():
     parser = argparse.ArgumentParser(description='Use LinkedIn')
     parser.add_argument('--topic', type=str, help='search term. Replace spaces with _')
-    parser.add_argument('--username', type=str,
-                        help='LinkedIn Username')
-    parser.add_argument('--password', type=str,
-                        help='LinkedIn User password')
-    parser.add_argument('--driver-path', type=str, default='/usr/local/bin/chromedriver',
-                        help='Path to chromedriver')
-    parser.add_argument('--login-url', type=str, default='https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin',
-                        help='Path to URL page')
-    parser.add_argument('--limit', type=int, default=3,
-                        help='number of google pages to limit search to')
-    parser.add_argument('--kafka-host', type=str, default="localhost",
-                        help='kafka host')
-    parser.add_argument('--kafka-port', type=int, default=29092,
-                        help='kafka port')
+    parser.add_argument('--username', type=str, help='LinkedIn Username')
+    parser.add_argument('--password', type=str, help='LinkedIn User password')
+    parser.add_argument('--driver-path', type=str, default='/usr/local/bin/chromedriver', help='Path to chromedriver')
+    parser.add_argument('--login-url', type=str, default='https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin', help='Path to URL page')
+    parser.add_argument('--limit', type=int, default=3, help='number of google pages to limit search to')
+    parser.add_argument('--kafka-host', type=str, default="localhost", help='kafka host')
+    parser.add_argument('--kafka-port', type=int, default=29092, help='kafka port')
     parser.add_argument('--backup', action='store_true', help='output to local json file <topic>_output.json')
     
     args = parser.parse_args()
     return args.topic, args.username, args.password, args.driver_path, args.login_url, args.limit, args.kafka_host, args.kafka_port, args.backup
 
+'''
+Serialize dict for publishing to kafka
+'''
 def serialize(dictionary):
     return json.dumps(dictionary).encode('ascii')
     
@@ -63,13 +55,15 @@ class LinkedInScraper:
         self.password=password
         self.login_url = login_url
         option = webdriver.ChromeOptions()
-        option.add_argument(" — incognito")
+        option.add_argument(" — incognito") # use incognito mode
         self.browser = webdriver.Chrome(executable_path=driverpath, chrome_options=option)
         
     def loginToLinkedIn(self):
         self.browser.get(self.login_url)
         username_input = self.browser.find_element_by_id("username")
         username_input.send_keys(username)
+        
+        # wait upwards of 5 seconds before moving to password
         time.sleep(random.random() * 5)
         
         password_input = self.browser.find_element_by_id("password")
@@ -77,10 +71,17 @@ class LinkedInScraper:
         
         XPATH="/html/body/div/main/div/form/div[contains(@class, 'login__form_action_container')]/button"
         login_button = self.browser.find_element_by_xpath(XPATH)
+        
+        # wait upwards of 3 seconds before submitting
         time.sleep(random.random() * 3)
         login_button.click()
+        
+        # Wait 4 seconds for next page to load
         time.sleep(4)
     
+    '''
+    Search Google for relevant LinkedIn Profiles
+    '''
     def searchForRelevantLIProfiles(self, query, page_limit=3):
         count = 0           # for keeping track of pagination
         users_links = []    # initialize empty list for urls to user pages
@@ -103,71 +104,100 @@ class LinkedInScraper:
             # organize all hrefs to the users_links list
             users_links.extend([result.find_element_by_tag_name('a').get_attribute('href') for result in results])
             
-            # go to next page
+            # gincrement page count
             count += 1
+            # if enough pages have been viewed, terminate loop
             if count == page_limit:
                 break
             next_button = self.browser.find_element_by_id('pnnext')
             next_page_link = next_button.get_attribute('href')
             self.browser.get(next_page_link)
         
-        #print(users_links)
         return users_links
     
+    '''
+    Find the user's given location from their profile
+    '''
     def scrapeLocation(self):
         try:
             location = self.browser.find_element_by_class_name('pv-top-card').find_element_by_xpath('div[2]/div[2]/div[1]/ul[2]/li[1]').text
+        
+        # If the element is not available, the page does not exist (Google shows a cached page)
         except selenium.common.exceptions.NoSuchElementException:
+            # Set location to false so downstream login knows to skip the given user
             location = False
         return location
     
+    '''
+    Find the user's listed skills
+    '''
     def scrapeSkills(self):
         top_skills_list = []
         skills_list = []
-        # scroll to bottom
-        #self.browser.execute_script("window.scrollTo(0, 500);") #document.body.scrollHeight);")
-        # wait for new content to generate after scrolling
+        # Wait a second
         time.sleep(1)
-        # get skills section
+        
+        # Scroll downwards until the skills section is available
         scroll=500
         while True:
             try:
                 skills_section = self.browser.find_element_by_class_name('pv-skill-categories-section')
-                break
+                break # end loop when section is found
+                
             except selenium.common.exceptions.NoSuchElementException:
+                # scroll down
                 self.browser.execute_script("window.scrollTo(0, {});".format(scroll))
+                
+                # increment scroll postion for next pass
                 scroll += 500
+                
+                # if we have scrolled down over 10 times, the skills section is not populated.
+                #   so return the empty lists
                 if scroll > 5000:
                     return top_skills_list, skills_list
+                
+                # pause for 1-2 seconds between scroll attempts
                 time.sleep(random.random() + 1)
+                
+        # once skills section is found, grab the top 3 skills
         subsection = skills_section.find_element_by_tag_name('ol')
         top_skills = subsection.find_elements_by_tag_name('li')
-        #print(len(top_skills))
-        #print("Top Skills:")
+        
+        
         for top_skill in top_skills:
             try:
                 skill = top_skill.find_element_by_xpath('div/div/p/a/span').text
-         #       print(skill)
+                
+                # add skill to the list
                 top_skills_list.append(skill)
             except selenium.common.exceptions.NoSuchElementException:
                 continue
             
-        #print("expanding to get more...")
+        # add top skills to the skills list
         skills_list.extend(top_skills_list)
+        
+        # expand to show more skills if possible
         try:
             show_more_button = skills_section.find_element_by_xpath('div[2]/button')
             show_more_button.click()
+            
+            # Pause 1 second to let expansion happen
             time.sleep(1)
-            #print('Other skills:')
+            
+            # get expanded section
             expanded_section = self.browser.find_element_by_id('skill-categories-expanded')
             for x in expanded_section.find_elements_by_tag_name('div'):
                 for area in x.find_elements_by_tag_name('li'):
                     try:
                         skill = area.find_element_by_xpath('div/div/p/a/span').text
-                        #print(skill)
+                        
+                        # add skill to list
                         skills_list.append(skill)
                     except selenium.common.exceptions.NoSuchElementException:
+                        # some li-s don't have that xpath, but they do not contain skills, so skip them
                         continue
+                    
+        # account for further skills not being populated
         except selenium.common.exceptions.NoSuchElementException:
             pass
         except selenium.common.exceptions.ElementClickInterceptedException:
@@ -175,28 +205,39 @@ class LinkedInScraper:
         
         return top_skills_list, skills_list
     
+    '''
+    Find the user's listed languages, if possiblt
+    '''
     def scrapeLanguages(self):
         langs = []
         # scroll to bottom
         self.browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         # wait for new content to generate after scrolling
         time.sleep(1)
-        #print('Languages:')
+        
         try:
             lang_section = self.browser.find_element_by_id('languages-expandable-content').find_element_by_xpath('ul')
             for x in lang_section.find_elements_by_tag_name('li'):
-                #print(x.text)
+                
+                # add language to list
                 langs.append(x.text)
+        
+        # account for languages not being there
         except selenium.common.exceptions.NoSuchElementException:
             pass
         return langs
     
+    '''
+    Scrape user's page for desired info
+    '''
     def scrapePage(self, url):
         self.browser.get(url)
         time.sleep(random.random()*3 + 1)
         page_info = {}
         page_info['id'] = sha256(url.encode('utf-8')).hexdigest()
         location = self.scrapeLocation()
+        
+        # if location is not False, the page exists, so continue
         if location:
             page_info['location'] = location
             top_skills, skills = self.scrapeSkills()
@@ -204,30 +245,52 @@ class LinkedInScraper:
             page_info['skills'] = skills
             page_info['languages'] = self.scrapeLanguages()
             return page_info
+        
+        # if location is False, the page does not exist, so inform the downstream logic
         else:
             return False
         
 
 if __name__ == '__main__':
     topic, username, password, driverpath, url, page_limit, host, port , backup = parseArgs()
+    
+    # Setup Kafka producer
     producer = KafkaProducer(bootstrap_servers=["{}:{}".format(host,port)])
     
+    # Initialize LinkedInScraper object
     LIS = LinkedInScraper(username, password, driverpath, url)
+    
+    # construct the google query from the topic
     query = '"{}"'.format(' '.join(topic.split('_')))
+    
+    # search for the relevant users
     relevant_users = LIS.searchForRelevantLIProfiles(query, page_limit=page_limit)
+    
+    # login to the linkedin account
     LIS.loginToLinkedIn()
-    for user_link in relevant_users[48:]:
+    
+    # for each user
+    for user_link in relevant_users:
+        # print the url for debugging purposes
         print(user_link)
+        
+        # get info from page
         info = LIS.scrapePage(user_link)
+        
+        # if 'info' is not false, page was successfully scraped, so if it is False, skip this user
         if not info:
             continue
+        
+        # if --backup was passed in, write results to local file
         if backup:
             with open('{}_output.json'.format(topic), 'a') as fObj:
                 fObj.write(json.dumps(info))
                 fObj.write('\n')
+        
+        # publish to Kafka topic
         producer.send(topic, serialize(info))
         
-    # flush data to topic at end
+    # flush data to topic at end to ensure all message get through
     producer.flush()
         
 
