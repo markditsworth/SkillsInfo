19cadacec185bc333a848e5cc550e90b5dd35ee9
Mark Ditsworth <markditsworth@protonmail.com>
2020-04-08 13:28:05 -0500

    able to deploy parallel containers

--- logstash/run.sh
+++ logstash/run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+topic=$1
+# template out pipeline file
+sed "s/TOPIC/${topic}/g" pipeline_template.conf > pipeline_${topic}.conf
 
-docker run -it --name logstash --network kafka-network -v $(pwd)/logstash.yml:/usr/share/logstash/config/logstash.yml -v $(pwd)/pipeline.conf:/usr/share/logstash/pipeline/logstash.conf logstash
+# spin up logstash container with new config file
+docker run -it --name logstash_${topic} --network kafka-network -v $(pwd)/logstash.yml:/usr/share/logstash/config/logstash.yml -v $(pwd)/pipeline_${topic}.conf:/usr/share/logstash/pipeline/logstash.conf logstash
