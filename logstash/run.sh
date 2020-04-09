9c414bcc1d086051282704b97885658cc6b8866b
Mark Ditsworth <markditsworth@protonmail.com>
2020-04-09 13:46:59 -0500

    run detached

--- logstash/run.sh
+++ logstash/run.sh
@@ -4,4 +4,4 @@ topic=$1
 sed "s/TOPIC/${topic}/g" pipeline_template.conf > pipeline_${topic}.conf
 
 # spin up logstash container with new config file
-docker run -it --name logstash_${topic} --network kafka-network -v $(pwd)/logstash.yml:/usr/share/logstash/config/logstash.yml -v $(pwd)/pipeline_${topic}.conf:/usr/share/logstash/pipeline/logstash.conf logstash
+docker run -itd --name logstash_${topic} --network kafka-network -v $(pwd)/logstash.yml:/usr/share/logstash/config/logstash.yml -v $(pwd)/pipeline_${topic}.conf:/usr/share/logstash/pipeline/logstash.conf logstash
